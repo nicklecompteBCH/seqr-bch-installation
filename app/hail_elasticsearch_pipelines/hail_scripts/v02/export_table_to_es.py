3f864fcadb3265f69c10627b03cb659eb792df19
Nick LeCompte <nicklecompte@OSXLAP04956.local>
2019-10-07 21:39:20 -0400

    load clinvar py3

--- app/hail_elasticsearch_pipelines/hail_scripts/v02/export_table_to_es.py
+++ app/hail_elasticsearch_pipelines/hail_scripts/v02/export_table_to_es.py
@@ -4,14 +4,52 @@ import hail as hl
 
 from hail_scripts.v02.utils.elasticsearch_client import ElasticsearchClient
 
-def add_sample_field_to_vds(vds : hl.Table, field_name, field_filter):
-    vds.annotate(field_name = set(map(lambda x: x.sample_id, filter(field_filter, vds))))
+"""
+From the hail docs:
+
+Col field is sample id
+
+Row Fields
+
+locus (tlocus or tstruct) – Row key.
+    The chromosome (CHROM field) and position (POS field).
+    If reference_genome is defined, the type will be
+        tlocus parameterized by reference_genome.
+    Otherwise, the type will be a tstruct with two fields:
+        contig with type tstr
+        position with type tint32.
+
+alleles (tarray of tstr) –
+    Row key. An array containing the alleles of the variant.
+    The reference allele (REF field) is the first element in the array
+    and the alternate alleles (ALT field) are the subsequent elements.
+
+filters (tset of tstr) – Set containing all filters applied to a variant.
+
+rsid (tstr) – rsID of the variant.
+
+qual (tfloat64) – Floating-point number in the QUAL field.
+
+info (tstruct) – All INFO fields defined in the VCF header can be found in the struct info.
+    Data types match the type specified in the VCF header,
+    and if the declared Number is not 1, the result will be stored as an array.
+
+"""
+
+def add_sample_metadata_to_table(tb: hl.Table) -> hl.Table:
+    if 'vep' not in set(tb.row):
+        # Probably should have never been called in the first place...
+        # but no harm no foul.
+        return tb
+    # otherwise we want to add the sample_num_alt fields to the VEP struct
+    #
+
 
 
 def export_table_to_elasticsearch(ds: hl.Table, host, index_name, index_type, is_vds  = False, port=9200, num_shards=1, block_size=200):
     if is_vds:
         for i in range(1,3):
-            ds = add_sample_field_to_vds(ds, f"sample_num_alt_{i}",(lambda x: x.num_alt == i))
+            ds = add_sample_field_to_vds(ds, f"sample_num_alt_{i}",ds.filter(ds.))
     es = ElasticsearchClient(host, port)
     es.export_table_to_elasticsearch(
         ds,
