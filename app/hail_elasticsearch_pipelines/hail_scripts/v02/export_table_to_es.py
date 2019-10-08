00207dd4f1ade1c294d7dd222840b3fa90cb0f8d
Nick LeCompte <nicklecompte@OSXLAP04956.local>
2019-10-07 23:49:47 -0400

    oops

--- app/hail_elasticsearch_pipelines/hail_scripts/v02/export_table_to_es.py
+++ app/hail_elasticsearch_pipelines/hail_scripts/v02/export_table_to_es.py
@@ -47,7 +47,7 @@ def add_sample_metadata_to_table(tb: hl.Table) -> hl.Table:
         if hl.len(tb.alleles) > 2:
             tb = tb.annotate(samples_num_alt_2 = tb.alleles[2])
         if hl.len(tb.alleles) > 3:
-        tb = tb.annotate(samples_num_alt_3 = tb.alleles[3])
+            tb = tb.annotate(samples_num_alt_3 = tb.alleles[3])
         return tb
 
 
