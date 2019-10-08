c0f014607a3b9633c5d24ad932d52a3529584277
Nick LeCompte <nicklecompte@OSXLAP04956.local>
2019-10-07 23:44:17 -0400

    oops

--- app/hail_elasticsearch_pipelines/hail_scripts/v02/export_table_to_es.py
+++ app/hail_elasticsearch_pipelines/hail_scripts/v02/export_table_to_es.py
@@ -44,7 +44,7 @@ def add_sample_metadata_to_table(tb: hl.Table) -> hl.Table:
     # otherwise we want to add the sample_num_alt fields to the VEP struct
     else:
         tb = tb.annotate(samples_num_alt_1 = tb.alleles[1])
-        tb = tb.annotate(samples_num_alt_2 = tb.alleles[2] if hl.len(tb.alleles))
+        tb = tb.annotate(samples_num_alt_2 = tb.alleles[2] if hl.len(tb.alleles) > 2)
         tb = tb.annotate(samples_num_alt_3 = tb.alleles[3] if hl.len(tb.alleles) > 3)
         return tb
 
