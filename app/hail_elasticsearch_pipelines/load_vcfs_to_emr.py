a3b4c0a7959bc08553c2ada6b6bf72081307b857
Nick LeCompte <nicklecompte@osxlap04956.tch.harvard.edu>
2019-10-08 11:19:31 -0400

    cv

--- app/hail_elasticsearch_pipelines/load_vcfs_to_emr.py
+++ app/hail_elasticsearch_pipelines/load_vcfs_to_emr.py
@@ -337,34 +337,34 @@ def add_project_dataset_to_elastic_search(
         ),
     )
 
-    review_status_str = hl.delimit(hl.sorted(hl.array(hl.set(clinvar_mt.info.CLNREVSTAT)), key=lambda s: s.replace("^_", "z")))
+    #review_status_str = hl.delimit(hl.sorted(hl.array(hl.set(clinvar_mt.info.CLNREVSTAT)), key=lambda s: s.replace("^_", "z")))
 
-    vep_mt = vep_mt.annotate_rows(
-            allele_id=clinvar_mt.info.ALLELEID,
-            alt=get_expr_for_alt_allele(vep_mt),
-            chrom=get_expr_for_contig(vep_mt.locus),
-            clinical_significance=hl.delimit(hl.sorted(hl.array(hl.set(clinvar_mt.info.CLNSIG)), key=lambda s: s.replace("^_", "z"))),
-            domains=get_expr_for_vep_protein_domains_set(vep_transcript_consequences_root=vep_mt.vep.transcript_consequences),
-            gene_id_to_consequence_json=get_expr_for_vep_gene_id_to_consequence_map(
-                vep_sorted_transcript_consequences_root=vep_mt.sortedTranscriptConsequences,
-                gene_ids=vep_mt.gene_ids
-            ),
-            #gold_stars=CLINVAR_GOLD_STARS_LOOKUP[review_status_str],
-            #**{f"main_transcript_{field}": vep_mt.main_transcript[field] for field in vep_mt.main_transcript.dtype.fields},
-            pos=get_expr_for_start_pos(vep_mt),
-            ref=get_expr_for_ref_allele(vep_mt),
-            #review_status=review_status_str,
-            # transcript_consequence_terms=get_expr_for_vep_consequence_terms_set(
-            #     vep_transcript_consequences_root=vep_mt.sortedTranscriptConsequences
-            # ),
-            # transcript_ids=get_expr_for_vep_transcript_ids_set(
-            #     vep_transcript_consequences_root=vep_mt.sortedTranscriptConsequences
-            # ),
-            #transcript_id_to_consequence_json=get_expr_for_vep_transcript_id_to_consequence_map(
-            #    vep_transcript_consequences_root=vep_mt.sortedTranscriptConsequences
-            #)
-            #variant_id=get_expr_for_variant_id(vep_mt),
-        )
+    # vep_mt = vep_mt.annotate_rows(
+    #         allele_id=clinvar_mt.info.ALLELEID,
+    #         alt=get_expr_for_alt_allele(vep_mt),
+    #         chrom=get_expr_for_contig(vep_mt.locus),
+    #         clinical_significance=hl.delimit(hl.sorted(hl.array(hl.set(clinvar_mt.info.CLNSIG)), key=lambda s: s.replace("^_", "z"))),
+    #         domains=get_expr_for_vep_protein_domains_set(vep_transcript_consequences_root=vep_mt.vep.transcript_consequences),
+    #         gene_id_to_consequence_json=get_expr_for_vep_gene_id_to_consequence_map(
+    #             vep_sorted_transcript_consequences_root=vep_mt.sortedTranscriptConsequences,
+    #             gene_ids=vep_mt.gene_ids
+    #         ),
+    #         #gold_stars=CLINVAR_GOLD_STARS_LOOKUP[review_status_str],
+    #         #**{f"main_transcript_{field}": vep_mt.main_transcript[field] for field in vep_mt.main_transcript.dtype.fields},
+    #         pos=get_expr_for_start_pos(vep_mt),
+    #         ref=get_expr_for_ref_allele(vep_mt),
+    #         #review_status=review_status_str,
+    #         # transcript_consequence_terms=get_expr_for_vep_consequence_terms_set(
+    #         #     vep_transcript_consequences_root=vep_mt.sortedTranscriptConsequences
+    #         # ),
+    #         # transcript_ids=get_expr_for_vep_transcript_ids_set(
+    #         #     vep_transcript_consequences_root=vep_mt.sortedTranscriptConsequences
+    #         # ),
+    #         #transcript_id_to_consequence_json=get_expr_for_vep_transcript_id_to_consequence_map(
+    #         #    vep_transcript_consequences_root=vep_mt.sortedTranscriptConsequences
+    #         #)
+    #         #variant_id=get_expr_for_variant_id(vep_mt),
+    #     )
     # add clinvar
     #vep_mt = vep_mt.union_cols(clinvar_mt)
     export_table_to_elasticsearch(vep_mt.rows(), host, index_name+"vep", index_type, is_vds=True, port=port,num_shards=num_shards, block_size=block_size)
