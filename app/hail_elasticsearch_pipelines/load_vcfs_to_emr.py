b739da1bbd519e9617911260684719b69c2ddc87
Nick LeCompte <nicklecompte@osxlap04956.tch.harvard.edu>
2019-10-08 13:17:20 -0400

    fixes

--- app/hail_elasticsearch_pipelines/load_vcfs_to_emr.py
+++ app/hail_elasticsearch_pipelines/load_vcfs_to_emr.py
@@ -354,7 +354,7 @@ def add_project_dataset_to_elastic_search(
             'num_alt' : vep_mt.info.NS,
             'ab':vep_mt.info.AO,
             'dp': vep_mt.info.DP,
-            'sample_id': hl.literal(parse_vcf_s3_path(s3path_to_vcf)['filename'])
+            'sample_id': hl.literal(parse_vcf_s3_path(dataset.vcf_s3_path)['filename'])
         })
     )
 
