cf1d7b15000f2f86466fa5cca08f065d9a1ca724
Nicholas LeCompte <nicholas.lecompte@childrens.harvard.edu>
2020-02-09 06:19:44 -0500

    typo

--- app/load_vcfs_to_emr.py
+++ app/load_vcfs_to_emr.py
@@ -109,7 +109,7 @@ def add_vep_to_vcf(mt):
 def load_hgmd_vcf():
 
     mt = import_vcf(
-        's3n://seqr-resources/GRCh37/hgmd/hgmd_pro_2018.4_hg19.vcf.gz,
+        's3n://seqr-resources/GRCh37/hgmd/hgmd_pro_2018.4_hg19.vcf.gz',
         "37",
         "hgmd_grch37",
     )
