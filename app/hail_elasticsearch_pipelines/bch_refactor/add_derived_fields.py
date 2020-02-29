f8c381dddab9add05b7ebd7f3fd22c6bc4867aed
Nicholas LeCompte <nicholas.lecompte@childrens.harvard.edu>
2020-02-29 00:35:46 -0500

    typo

--- app/hail_elasticsearch_pipelines/bch_refactor/add_derived_fields.py
+++ app/hail_elasticsearch_pipelines/bch_refactor/add_derived_fields.py
@@ -28,7 +28,7 @@ def annotate_mt_with_derived_fields(mt: hl.MatrixTable) -> hl.MatrixTable:
 
 
     mt = mt.annotate_rows(
-            main_transcript=get_expr_for_worst_transcript_consequence_annotations_struct(
+            mainTranscript=get_expr_for_worst_transcript_consequence_annotations_struct(
                     vep_sorted_transcript_consequences_root=mt.sortedTranscriptConsequences
                 ),
             gene_ids=get_expr_for_vep_gene_ids_set(
